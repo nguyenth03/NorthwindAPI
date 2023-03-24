8f7497b73297a7f1dd75b0cea1af7d833cd4e782
nguyenth03 <117792212+nguyenth03@users.noreply.github.com>
2023-03-24 17:13:12 +0700

    Controller_NorthWind

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using NorthWindAPI.Models;
+using System.Configuration;
+
+namespace NorthWindAPI.Controllers
+{
+    public class CategoriesController : ApiController
+    {
+        string strconn = ConfigurationManager.ConnectionStrings["NW"].ConnectionString;
+
+        [HttpGet]
+        [Route("api/IndexCategory")]
+        public List<Category> GetCategories()
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            string Sql = "Select * from Categories";
+            SqlDataAdapter da = new SqlDataAdapter(Sql,conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Categories");
+            List<Category> categories = new List<Category>();
+            foreach(DataRow i in ds.Tables["Categories"].Rows)
+            {
+                Category c = new Category();
+                c.CategoryID = (int)i["CategoryID"];
+                c.CategoryName = i["CategoryName"].ToString();
+                categories.Add(c);
+            }
+            return categories;
+        }
+
+        [HttpPost]
+        [Route("api/Detail")]
+        public DataSet GetCategoryID(int ID)
+        {
+            SqlConnection s = new SqlConnection(strconn);
+            string sql = "Select * from Categories where CategoryID =" + ID;
+            SqlDataAdapter da = new SqlDataAdapter(sql, s);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Categories");
+            return ds;
+        }
+
+        [HttpPost]
+        [Route("api/SearchCategory")]
+        public List<Category> Search(string str)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            string sql = "Select * from Categories where CategoryName like '%" + str + "%'";
+            SqlDataAdapter da = new SqlDataAdapter(sql,conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Categories");
+            List<Category> ListC = new List<Category>();
+            foreach(DataRow dL in ds.Tables["Categories"].Rows)
+            {
+                Category c = new Category();
+                c.CategoryID = (int)dL["CategoryID"];
+                c.CategoryName = (string)dL["CategoryName"];
+                ListC.Add(c);
+            }
+            return ListC;
+        }
+        [HttpPost]
+        [Route("api/Category/Insert")]
+        public int InsertCategory(string CategoryName, string Description)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            string Sql = "insert into Categories (CategoryName,Description) values (N'" + CategoryName + "',N'" + Description + "')";
+            cmd.CommandText = Sql;
+            return 0;
+        }
+
+        [HttpPost]
+        [Route("api/Categories/Update")]
+        public int UpdateCategory(int id,string CategoryName, string Description)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            string Sql = "Update Categories  set CategoryName =N'" + CategoryName + "',Description =N'" + Description + "' where CategoryID=" + id;
+            cmd.CommandText = Sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return 1;
+            }catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/Delete")]
+        public int DeleteCategory(int id)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            string sql = "Delete from Categories where CategoryID =" + id;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return 1;
+            }catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ Controllers/USERSController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using NorthWindAPI.Models;
+using System.Configuration;
+
+namespace NorthWindAPI.Controllers
+{
+    public class USERSController : ApiController
+    {
+        string strconn = ConfigurationManager.ConnectionStrings["NW"].ConnectionString;
+
+        [HttpGet]
+        public List<USER> GetAllUser()
+        {
+            SqlConnection sqlConn = new SqlConnection(strconn);
+            string sql = "Select * from USERS";
+            SqlDataAdapter adapter = new SqlDataAdapter(sql,sqlConn);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds,"USERS");
+            List<USER> lusers = new List<USER>();
+            foreach( DataRow use in ds.Tables["USERS"].Rows)
+            {
+                USER u = new USER();
+                u.UserID = (int)use["UserID"];
+                u.UserName = use["UserName"].ToString();
+                u.Password = use["Password"].ToString();
+                u.Description   = use["Description"].ToString();
+                lusers.Add(u);
+            }
+            return lusers;
+
+        }
+
+        [HttpPost]
+        public DataSet GetCategoryID(int ID)
+        {
+            SqlConnection conn= new SqlConnection(strconn);
+            String sql = "Select * from USERS where UserID = " + ID;
+            SqlDataAdapter adapter = new SqlDataAdapter(sql,conn);
+            DataSet d = new DataSet();
+            adapter.Fill(d, "USERS");
+            return d;
+        }
+
+        [HttpPut]
+        public int InserCategory(string name,string pwd ,string discription)
+        {
+            SqlConnection strcon = new SqlConnection(strconn);
+            string sql = "Insert into USERS(UserName,Password,Description) values(N'" + name + "',N'" + pwd + "',N'" + discription + "')";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = strcon;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        [HttpPut]
+        public int UpdateCategory(int id ,string name,string pwd,string discription)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            string sql = "Update USERS set UserName=N'" + name + "',Password=N'" + pwd + "',Description=N'" + discription + "'";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return 1;
+            }catch(Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        [HttpDelete]
+        public int DeleteCategory(int id)
+        {
+            SqlConnection conn = new SqlConnection(strconn);
+            string sql = "delete from USERS where UserID =" + id;
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = sql;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return 1;
+            }catch(Exception ex) { return 0; }
+        }
+    }
+}
